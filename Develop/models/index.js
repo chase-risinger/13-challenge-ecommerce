6fc2a71fc1d82a61ef0895777b71f3b43225597e
chase-risinger <ccr1988@gmail.com>
2021-12-17 21:44:19 -0600

    beginning tag routes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -17,13 +17,13 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'hash_tag',
+  as: 'product_tag',
   foreignKey: 'product_id'
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'Product_name',
+  as: 'product_tag',
   foreignKey: 'tag_id'
 });
 
